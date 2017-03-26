0867c9cd0861097c152153a82db2aca861af0789
SkiboPavlo <skibo@i.ua>
2017-03-27 00:02:40 +0300

    add cart, associations and  some ui

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,23 @@ class Product < ApplicationRecord
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\z}i,
-    message: 'URL повинен вказувати на зображення формату GIF, JPG или PNG.'
-  }
+    message: 'URL повинен вказувати на зображення формату GIF, JPG или PNG.' }
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
 
   def self.latest
     Product.order(:updated_at).last
+  end
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'There are headings')
+      return false
+    end
   end
 end
